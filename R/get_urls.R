ce07e028f4833077b5bd8ec8adfaca35fd006c5a
bczekanski <Ben@Bens-MacBook-Pro.local>
2016-12-29 16:24:10 -0500

    get_urls

---
+++ R/get_urls.R
@@ -0,0 +1,23 @@
+get_urls <- function(){}
+
+  library(wcgrads)
+  library(dplyr)
+  url01 <- ('http://web.williams.edu/admin/registrar//catalog/depts0001/catalog.pdf')
+  url02 <- ('http://web.williams.edu/admin/registrar//catalog/depts0102/catalog.pdf')
+  url03 <- ('http://web.williams.edu/admin/registrar//catalog/depts0203/catalog.pdf')
+  url04 <- ('http://web.williams.edu/admin/registrar//catalog/depts0304/catalog.pdf')
+  url05 <- ('http://web.williams.edu/admin/registrar//catalog/depts0405/catalog0405.pdf')
+  url06 <- ('http://web.williams.edu/admin/registrar//catalog/catalog0506.pdf')
+  url07 <- ('http://web.williams.edu/admin/registrar//catalog/catalog0607.pdf')
+  url08 <- ('http://web.williams.edu/admin/registrar//catalog/catalog0708.pdf')
+  url09 <- ('http://web.williams.edu/admin/registrar//catalog/catalog0809.pdf')
+  url10 <- ('http://web.williams.edu/admin/registrar//catalog/catalog0910.pdf')
+  url11 <- ('http://web.williams.edu/admin/registrar//catalog/bulletin2010-11.pdf')
+  url12 <- ('http://web.williams.edu/admin/registrar//catalog/bulletin2011_12.pdf')
+  url13 <- ('http://web.williams.edu/admin/registrar//catalog/bulletin12_13.pdf')
+  url14 <- ('http://web.williams.edu/admin/registrar//catalog/bulletin2013_14.pdf')
+  url15 <- ('http://web.williams.edu/admin/registrar//catalog/bulletin2014_15.pdf')
+  url16 <- ('http://web.williams.edu/admin/registrar//catalog/bulletin2015_16.pdf')
+  url17 <- ('http://web.williams.edu/admin/registrar//catalog/bulletin2016_17.pdf')
+
+}
