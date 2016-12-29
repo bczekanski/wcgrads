2b1688959c4a6dc7c49f1ed710af126be11ac020
bczekanski <Ben@Bens-MacBook-Pro.local>
2016-12-29 16:45:03 -0500

    download not dowload

---
+++ R/download_pdfs.R
@@ -0,0 +1,21 @@
+download_pdfs <- function(){
+
+`01` <- download.file(url01, '01.pdf')
+`02` <- download.file(url02, '02.pdf')
+`03` <- download.file(url03, '03.pdf')
+`04` <- download.file(url04, '04.pdf')
+`05` <- download.file(url05, '05.pdf')
+`06` <- download.file(url06, '06.pdf')
+`07` <- download.file(url07, '07.pdf')
+`08` <- download.file(url08, '08.pdf')
+`09` <- download.file(url09, '09.pdf')
+`10` <- download.file(url10, '10.pdf')
+`11` <- download.file(url11, '11.pdf')
+`12` <- download.file(url12, '12.pdf')
+`13` <- download.file(url13, '13.pdf')
+`14` <- download.file(url14, '14.pdf')
+`15` <- download.file(url15, '15.pdf')
+`16` <- download.file(url16, '16.pdf')
+`17` <- download.file(url17, '17.pdf')
+
+}
