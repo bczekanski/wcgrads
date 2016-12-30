b8fcc5c0e5bd34f40267954c74c0fd8cb03355ae
bczekanski <Ben@Bens-MacBook-Pro.local>
2016-12-29 20:54:26 -0500

    general update

---
+++ R/combine_data.R
@@ -0,0 +1,48 @@
+combine_data <- function(){
+
+library(wcgrads)
+library(dplyr)
+
+`01` <- get_data('01') %>%
+  mutate(year = "00-01")
+`02` <- get_data('02') %>%
+  mutate(year = "01-02")
+`03` <- get_data('03') %>%
+  mutate(year = "02-03")
+`04` <- get_data('04') %>%
+  mutate(year = "03-04")
+`05` <- get_data('05') %>%
+  mutate(year = "04-05")
+`06` <- get_data('06') %>%
+  mutate(year = "05-06")
+`07` <- get_data('07') %>%
+  mutate(year = "06-07")
+`08` <- get_data('08') %>%
+  mutate(year = "07-08")
+`09` <- get_data('09') %>%
+  mutate(year = "08-09")
+`10` <- get_data('10') %>%
+  mutate(year = "09-10")
+`11` <- get_data('11') %>%
+  mutate(year = "10-11")
+`12` <- get_data('12') %>%
+  mutate(year = "11-12")
+`13` <- get_data('13') %>%
+  mutate(year = "12-13")
+`14` <- get_data('14') %>%
+  mutate(year = "13-14")
+`15` <- get_data('15') %>%
+  mutate(year = "14-15")
+`16` <- get_data('16') %>%
+  mutate(year = "15-16")
+`17` <- get_data('17') %>%
+  mutate(year = "16-17")
+
+all <- rbind(`01`, `02`, `03`, `04`, `05`, `06`, `07`, `08`, `09`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`)
+
+all$firstinit <- sub("^$", NA, all$firstinit)
+
+all <- all %>%
+  drop_na(firstinit, lastinit)
+
+}
---
+++ R/visual.R
@@ -0,0 +1,16 @@
+visual <- function(x) {
+
+library(wcgrads)
+library(dplyr)
+library(ggplot2)
+library(ggExtra)
+
+a <- x %>%
+  ggplot(aes(firstinit, lastinit)) +
+  geom_bin2d() +
+  scale_fill_gradient2()
+
+b <- ggMarginal(a, x, x$firstinit, x$lastinit, type = "histogram", margins = "both", stat = "count")
+b
+
+}
