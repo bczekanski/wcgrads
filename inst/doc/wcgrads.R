37b5b56bc4fa8a576dd714fb12a2ab7a2c5f5120
bczekanski <Ben@Bens-MacBook-Pro.local>
2017-01-30 20:16:08 -0500

    inst/doc

---
+++ inst/doc/wcgrads.R
@@ -0,0 +1,33 @@
+## ----warning = FALSE-----------------------------------------------------
+library(wcgrads)
+names <- namelength()
+head(names, 10)
+
+## ----fig.width = 5, fig.height = 3, warning = FALSE----------------------
+library(wcgrads)
+gradcount()
+
+## ----warning = FALSE-----------------------------------------------------
+library(wcgrads)
+latin_honors()
+
+## ----warning = FALSE-----------------------------------------------------
+library(wcgrads)
+total_latin_honors()
+
+## ----fig.width = 5, fig.height = 3, warning = FALSE----------------------
+library(wcgrads)
+pbk()
+
+## ----warning = FALSE-----------------------------------------------------
+library(wcgrads)
+dept_honors()
+
+## ----warning = FALSE-----------------------------------------------------
+library(wcgrads)
+latin_honors_gender()
+
+## ----warning = FALSE-----------------------------------------------------
+library(wcgrads)
+initials()
+
